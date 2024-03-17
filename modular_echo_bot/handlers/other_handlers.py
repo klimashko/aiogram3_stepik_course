5610d9a17aa2a867e6eab6de925b29ad96c43dc1
Evgenii Klimashko <klimashko108@gmail.com>
2024-03-17 21:56:18 +0300

    Измененны хендлеры в файлах, теперь они регистрируются через Router, то есть так как задумано в курсе

--- modular_echo_bot/handlers/other_handlers.py
+++ modular_echo_bot/handlers/other_handlers.py
@@ -1,7 +1,7 @@
 from aiogram import Router
-
 from aiogram.types import Message
-from lexicon.lexicon import LEXICON_RU
+from modular_echo_bot.lexicon.lexicon import LEXICON_RU
+
 
 # Инициализируем роутер уровня модуля
 router = Router()
