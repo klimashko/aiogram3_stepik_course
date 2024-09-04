503c7f45cdb55877ed3877c44223e8584d822c36
Evgenii Klimashko <klimashko108@gmail.com>
2024-09-05 00:43:47 +0300

    Внес изменения, чтобы в рамках метода bot.edit_massage_media посмотреть, что можно менять один вид media на другой, например audio на video

--- message_editing/media_editing.py
+++ message_editing/media_editing.py
@@ -6,12 +6,15 @@ from aiogram.types import (CallbackQuery, InlineKeyboardButton,
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 from aiogram.filters import CommandStart
 from aiogram.exceptions import TelegramBadRequest
+from environs import Env
 
-# Вместо BOT TOKEN HERE нужно вставить токен вашего бота,
-# полученный у @BotFather
-BOT_TOKEN = 'BOT TOKEN HERE'
 
-bot = Bot(BOT_TOKEN)
+env = Env() #Env() создает новый экземпляр класса Env, который предоставляет методы для работы с переменными среды.
+env.read_env() #загружает переменные среды из файла .env в объект env
+BOT_TOKEN = env.str("BOT_TOKEN") #присваивает переменной BOT_TOKEN значение переменной среды с именем BOT_TOKEN.
+
+# Создаем объекты бота и диспетчера
+bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
 
@@ -24,16 +27,16 @@ LEXICON: dict[str, str] = {
     'voice': '📢 Голосовое сообщение',
     'text_1': 'Это обыкновенное текстовое сообщение, его можно легко отредактировать другим текстовым сообщением, но нельзя отредактировать сообщением с медиа.',
     'text_2': 'Это тоже обыкновенное текстовое сообщение, которое можно заменить на другое текстовое сообщение через редактирование.',
-    'photo_id1': 'AgACAgIAAxkBAAIU5WPJSt9TUFzUwbCqewIUNtp9gJBYAAIJxTEbp5JJSvDAdS74pTGAAQADAgADcwADLQQ',
-    'photo_id2': 'AgACAgIAAxkBAAIU_WPJgB1KZiyEIu4OikbsEsJInzV-AALxxTEbp5JJSnu2IKkQdDDQAQADAgADcwADLQQ',
-    'voice_id1': 'AwACAgIAAxkBAAIU52PJSwWbxtBrhoL8o3njUdvNaHckAALZJwACp5JJStGYVdp4u5cILQQ',
-    'voice_id2': 'AwACAgIAAxkBAAIU9mPJfLxFoKpSTie3CZFL3PcFfTHhAAKWKQACp5JJSqoWOXKpDRntLQQ',
-    'audio_id1': 'CQACAgIAAxkBAAIVRWPKsPl83xynqlF9YvF5MRyF9GxeAAL1JAACkhBZSmyFCDY61yX8LQQ',
-    'audio_id2': 'CQACAgIAAxkBAAIVR2PKsXppkdhAnOlqwpOHDJivtfvJAAL4JAACkhBZSoMVyPSB59h5LQQ',
-    'document_id1': 'BQACAgIAAxkBAAIVEmPKY_5RcBfEMeQ55X02PNimd1FdAAJ1IwACkhBRSmszrVZWfOY6LQQ',
-    'document_id2': 'BQACAgIAAxkBAAIVFGPKZCYnp07PHr_OwXIKo5Z8fxcEAAJ3IwACkhBRSoZiZmUOrb4nLQQ',
-    'video_id1': 'BAACAgIAAxkBAAIVFmPKZL_cgfokLm3pBU5w3zspn3-lAAJ5IwACkhBRSs4Lk37jVF8xLQQ',
-    'video_id2': 'BAACAgIAAxkBAAIVGGPKZTURYRphKtnnVFHy8Oa6OxPXAAJ6IwACkhBRSnfUGQW2UKeBLQQ',
+    'photo_id1': 'AgACAgIAAxkBAAIJxGbXj6tApsGmx7uRvCoqdl0yK0XZAAIOAzIbI56YSrb93AABNHzBdAEAAwIAA3MAAzUE',
+    'photo_id2': 'AgACAgIAAxkBAAIJxmbXj7Lo3RtEgyIGOtSszb_iF3AUAAIPAzIbI56YSmdr-C6RAAHbEwEAAwIAA3MAAzUE',
+    'voice_id1': 'AwACAgIAAxkBAAIJymbXkChB40qtKIh_bO6boqzHHR8fAAKNSwACAYfBSo_ls5h26YqrNQQ',
+    'voice_id2': 'AwACAgIAAxkBAAIJzGbXkC4QTHmXEjFBTMtwe_PPDTAVAAKOSwACAYfBSm59ZZZi9UAxNQQ',
+    'audio_id1': 'CQACAgIAAxkBAAIJzmbXkDx2MjZFwIRtNeKvU6gPNbKtAAITVQACk0qZStnZOsqJ0jDjNQQ',
+    'audio_id2': 'CQACAgIAAxkBAAIJ0GbXkEI6Yd4nyDee2XzmESnZQDhUAAIWVQACk0qZSr2vBrAXo4ZDNQQ',
+    'document_id1': 'BQACAgIAAxkBAAIJ0mbXkFF5aPwmpSzWaW3LD-FVYZcDAAIQVQACk0qZSs1egsxOZjtINQQ',
+    'document_id2': 'BQACAgIAAxkBAAIJ1GbXkFbNERshUPlEzevpy1f9V9lkAAIRVQACk0qZSiuPg5JBqQH_NQQ',
+    'video_id1': 'BAACAgIAAxkBAAIJ1mbXkHWB7NXM92Keu5Z706Dmm2JKAAIVUgACL9aZSoTdmKmV8m-YNQQ',
+    'video_id2': 'BAACAgIAAxkBAAIJ2GbXkH01oiy4PbRVYgJiAsCoYqbPAAJCUgACL9aZSghlJDfCymQWNQQ',
 }
 
 
@@ -65,10 +68,10 @@ def get_markup(width: int, *args, **kwargs) -> InlineKeyboardMarkup:
 # Этот хэндлер будет срабатывать на команду "/start"
 @dp.message(CommandStart())
 async def process_start_command(message: Message):
-    markup = get_markup(2, 'photo')
-    await message.answer_photo(
-        photo=LEXICON['photo_id1'],
-        caption='Это фото 1',
+    markup = get_markup(2, 'audio')
+    await message.answer_audio(
+        audio=LEXICON['audio_id1'],
+        caption='Это аудио 1',
         reply_markup=markup
     )
 
@@ -78,14 +81,14 @@ async def process_start_command(message: Message):
     ['text', 'audio', 'video', 'document', 'photo', 'voice']
 ))
 async def process_button_press(callback: CallbackQuery, bot: Bot):
-    markup = get_markup(2, 'photo')
+    markup = get_markup(2, 'video')
     try:
         await bot.edit_message_media(
             chat_id=callback.message.chat.id,
             message_id=callback.message.message_id,
-            media=InputMediaPhoto(
-                media=LEXICON['photo_id2'],
-                caption='Это фото 2'
+            media=InputMediaAudio(
+                media=LEXICON['video_id2'],
+                caption='Это видео 2'
             ),
             reply_markup=markup
         )
@@ -93,9 +96,9 @@ async def process_button_press(callback: CallbackQuery, bot: Bot):
         await bot.edit_message_media(
             chat_id=callback.message.chat.id,
             message_id=callback.message.message_id,
-            media=InputMediaPhoto(
-                media=LEXICON['photo_id1'],
-                caption='Это фото 1'
+            media=InputMediaAudio(
+                media=LEXICON['audio_id1'],
+                caption='Это аудио 1'
             ),
             reply_markup=markup
         )
